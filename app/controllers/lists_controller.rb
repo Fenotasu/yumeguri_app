d39bf521e6630184d440f55414c2b8cd0bfd1f63
Fenotasu <aymarket888@gmail.com>
2018-06-13 21:12:08 +0900

    編集機能追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,32 @@
 class ListsController < ApplicationController
   def index
-    @lists=List.all
+    @lists=List.all.order(created_at: :desc)
   end
 
   def show
-   
     @list= List.find_by(id: params[:id])
   end
+
+def new
+end
+
+def create
+  @list=List.new(comment: params[:comment])
+  @list.save
+  redirect_to("/lists/index")
+end
+
+
+  def edit
+   @list=List.find_by(id: params[:id])
+  end
+
+def update
+  @list=List.find_by(id: params[:id])
+  @list.comment=params[:comment]
+  @list.save
+  redirect_to("/lists/index")
+
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'profile/signup'
   get 'lists/index'
+  get "lists/new"=>"lists#new"
   get"lists/:id" => "lists#show"
+
+  post "lists/create"=>"lists#create"
+  get"lists/:id/edit"=>"lists#edit"
+
+  post "lists/:id/update" => "lists#update"
+
   get '/' =>"home#top"
   get '/about'=>"home#about"
   get 'posts/:id' => "posts#show"
